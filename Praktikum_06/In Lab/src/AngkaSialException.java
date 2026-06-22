e063d9d1b2b625a7ed42a28f92afaa1e8eeef0bc
Ganendra Satya Sindhunata <ganendrasatyaa01@gmail.com>
2026-06-22 19:26:54 +0700

    Update AngkaSialException.java

--- Praktikum_06/In Lab/src/AngkaSialException.java
+++ Praktikum_06/In Lab/src/AngkaSialException.java
@@ -1,6 +1,6 @@
 //  Nama File : AngkaSialException.java
 //  Deskripsi : Eksepsi buatan sendiri, menolak masukan angka 13!
-//  Pembuat : Alyssa Shane Kurniawan - 24060124120038
+//  Pembuat : Ganendra Satya Sindhunata - 24060124120025
 //  Tanggal : 30 Maret 2026
 
 public class AngkaSialException extends Exception {
